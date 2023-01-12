dae9beae575f7334f8dce099ee43fade32b7323a
karthunni <98034897+karthunni@users.noreply.github.com>
2023-01-11 19:07:49 -0500

    Delete server directory

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express');
-const app = express();
-
-app.get("/api", (req, res) => {
-    res.json ({"animes": ["anime1", "anime2", "anime3","anime4", "anime5"]});
-})
-
-app.listen(5000, () => {console.log ("Server started on port 5000")});
